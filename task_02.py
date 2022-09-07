f718538bcbdba02547c5908fa34ec080bbb4fa7f
fanur <makhiyanov.ff@gmail.com>
2022-09-07 11:30:07 +0300

    Задача №2

--- task_02.py
+++ task_02.py
@@ -2,4 +2,10 @@
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-number = input('Введите число: ')
+from math import factorial
+
+n = int(input('Введите число: '))
+def f(x):
+    return ((x == 1) and 1) or x * factorial(x - 1)
+list2 = list(f(i) for i in range(1, n + 1))
+print(list2)
